d4eaa9632a574e7713f6b3e50ca156b524bc9734
tharindu-dm <85168846+tharindu-dm@users.noreply.github.com>
2024-08-03 17:02:11 +0530

    setup database connection

--- config/database.php
+++ config/database.php
@@ -1,8 +1,13 @@
 <?php
 
-// Database configuration
-//db = free-write-db
-username = "admin";
-password = "5xIQI2XQaZZh9nzP";
+return [
+    'driver' => 'mysql',
+    'host' => 'free-write-db.c5eaayaii022.eu-north-1.rds.amazonaws.com',
+    'database' => 'freewrite-db',
+    'username' => 'admin',
+    'password' => '5xIQI2XQaZZh9nzP',
+    'charset' => 'utf8mb4',
+    'strict' => true,
+];
 
 ?>
