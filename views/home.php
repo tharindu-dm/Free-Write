e2463f38b587c7287bb2f7e7efb972f5c78ad8e2
tharindu-dm <85168846+tharindu-dm@users.noreply.github.com>
2024-08-04 14:35:45 +0530

    added the error page and the feather icon

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<?php
+//a navbar with logo image link to home page, and a drop down brwose button, a search bar with placeholder and 2 buttons saying sighup or login
+
+// views/home.php
+
+require 'partials/header.php';
+
+?>
--- views/login.php
+++ views/login.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>
+        <link rel="icon" type="image/x-icon" href="/images/FeatherIcon.ico">
+        Login
+    </title>
     <link rel="stylesheet" href="/css/login.css">
 </head>
+
 <body>
     <form action="/login" method="post">
         <h2>Login</h2>
-        <?php if (isset($error)): ?>
+        <?php if (isset($error)) : ?>
             <p class="error"><?php echo htmlspecialchars($error); ?></p>
         <?php endif; ?>
-        
+
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
     </form>
 </body>
+
 </html>
