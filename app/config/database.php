96e07278e52c1def3b77bb47ffb331d19fc9d5ef
tharindu-dm <85168846+tharindu-dm@users.noreply.github.com>
2024-10-31 17:11:35 +0530

    EXTREME SECURITY FIX

--- app/config/database.php
+++ app/config/database.php
@@ -2,21 +2,49 @@
 
 trait Database
 {
-    private function connect() //connecting to azure sql server
+    private function connect($filePath) //connecting to azure sql server
     {
+        // Function to read and load the .env file
+        function loadEnv($filePath)
+        {
+            if (file_exists($filePath)) {
+                $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+                foreach ($lines as $line) {
+                    // Skip comments
+                    if (strpos(trim($line), '#') === 0) {
+                        continue;
+                    }
+                    // Split by '=' and trim whitespace
+                    list($key, $value) = explode('=', $line, 2);
+                    $key = trim($key);
+                    $value = trim($value);
+                    // Set the environment variable
+                    putenv("$key=$value");
+                }
+            }
+        }
+
+        // Load the .env file using the function
+
+        loadEnv(__DIR__ . '/.env');
+
+        // Now you can access the environment variables
+        $dsn = getenv('DB_DSN');
+        $username = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
+
         try {
-            $conn = new PDO("sqlsrv:server = tcp:freewrite-server.database.windows.net,1433; Database = Freewrite_db", "CloudSAbf7941bd", "7d9UCx9jTxhk");
+            $conn = new PDO($dsn, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
-        }
-        catch (PDOException $e) {
+        } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
     }
 
     public function query($query, $data = []) //using sql prepared statement to avoid sql injections
     {
-        $con = $this->connect();
+        $con = $this->connect(__DIR__ . '/.env');
         $statement = $con->prepare($query);
         $check = $statement->execute($data);
         if ($check) {
@@ -34,7 +62,7 @@ trait Database
     }
     public function get_row($query, $data = []) //one row 
     {
-        $con = $this->connect();
+        $con = $this->connect(__DIR__ . '/.env');
         $statement = $con->prepare($query);
 
         $check = $statement->execute($data);
