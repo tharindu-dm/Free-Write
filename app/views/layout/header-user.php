e8ecceae5ac4c11ef07027263fd1be70ee339216
Tharindu DM <tharindudm@hotmail.com>
2025-04-21 22:22:26 +0530

    writer should stay

--- app/views/layout/header-user.php
+++ app/views/layout/header-user.php
@@ -44,21 +44,19 @@
                         </div>
                     <?php endif; ?>
 
-                    <?php if (isset($_SESSION['user_type']) && ($_SESSION['user_type'] == 'reader' || $_SESSION['user_type'] == 'covdes')): ?>
-                        <div class="action-button">
-                            <form action="/Free-Write/public/Writer/DashboardNew" method="post">
-                                <div class="sign-in-btn">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                    </svg>
+                    <div class="action-button">
+                        <form action="/Free-Write/public/Writer/DashboardNew" method="post">
+                            <div class="sign-in-btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                </svg>
 
-                                    <button type="submit">&nbsp;Write </button>
-                                </div>
-                            </form>
-                        </div>
-                    <?php endif; ?>
+                                <button type="submit">&nbsp;Write </button>
+                            </div>
+                        </form>
+                    </div>
 
                     <div class="premium-notification-container">
                         <?php if ($_SESSION['user_type'] != 'admin' && $_SESSION['user_type'] != 'mod'): ?>
